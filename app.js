e29ef6485f798e909afa900c7800d2e4faeebdb5
eylul-commits <77168569+eylul-commits@users.noreply.github.com>
2023-06-04 22:07:25 +0300

    fix: undefined positive problem

--- app.js
+++ app.js
@@ -21,14 +21,15 @@ fetch("http://localhost:3000/api/basliklar")
 app.get('/:slug', (req,res)=>{
   console.log(req.params.slug)
     fetch("http://localhost:3000/api/baslik/" + req.params.slug).then(response => response.json()).then(async eksiData => {
-        for (let index = 0; index < eksiData.entries.length; index++) {
-          const result = await client.analyze("SentimentAnalysis", [eksiData.entries.at(index).body]);
-          eksiData.entries.at(index).sentiment = result[0].sentiment
-          eksiData.entries.at(index).positiveConfidence = JSON.stringify(result[0].confidenceScores.positive);
-          eksiData.entries.at(index).negativeConfidence = JSON.stringify(result[0].confidenceScores.negative);
-          eksiData.entries.at(index).neutralConfidence = JSON.stringify(result[0].confidenceScores.neutral);
-        }
+      for (let index = 0; index < eksiData.entries.length; index++) {
+        const result = await client.analyze("SentimentAnalysis", [eksiData.entries[index].body]);
+        eksiData.entries[index].sentiment = result[0].sentiment;
+        eksiData.entries[index].positiveConfidence = result[0]?.confidenceScores?.positive? JSON.stringify(result[0].confidenceScores.positive): "NA";
+        eksiData.entries[index].negativeConfidence = result[0]?.confidenceScores?.negative? JSON.stringify(result[0].confidenceScores.negative): "NA";
+        eksiData.entries[index].neutralConfidence = result[0]?.confidenceScores?.neutral? JSON.stringify(result[0].confidenceScores.neutral): "NA";
+      }
 
+        //to create a json file of our "entries"
         fs.writeFile('entries.json', JSON.stringify(eksiData, null, 2), (err) => {
           if (err) throw err;
           });
